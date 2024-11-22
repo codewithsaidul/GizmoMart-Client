1678d1099f43e35feba0dace75048e2bb632b983
codewithsaidul <codewithsaidul@gmail.com>
2024-11-22 23:40:34 +0600

    Product Data Fetch From DB

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,7 +1,21 @@
-import { products } from "../../data/Data";
+import { useQuery } from "@tanstack/react-query";
 import Button from "../Shared/Button";
+import axios from "axios";
+import Loading from "../Shared/Loading";
 
 const FeaturedProducts = () => {
+
+  const { data: products = [], isLoading} = useQuery({
+    queryKey: "products",
+    queryFn: async () => {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
+      return data;
+    }
+  })
+
+  // Showing Loading Screen If Data Not Found
+  if(isLoading) return <Loading />
+
   return (
     <div className="my-20">
       <h2 className="text-2xl font-bold text-slate-700 mb-10 text-center sm:text-left">
@@ -9,7 +23,7 @@ const FeaturedProducts = () => {
       </h2>
 
       {/* ============== Featured Product Container ===================== */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.slice(0, 6).map((product) => (
           <div
             key={product.id}
@@ -19,33 +33,33 @@ const FeaturedProducts = () => {
               <div>
                 <figure className="flex justify-center">
                   <img
-                    src={product.image}
-                    alt={product.name}
+                    src={product.productImage}
+                    alt={product.productName}
                     className="w-40 h-40 rounded-md"
                   />
                 </figure>
                 <div className="flex flex-col space-y-3 mt-6">
                   <h3 className="text-xl font-bold text-slate-700">
-                    {product.title}
+                    {product.productName}
                   </h3>
                   <p className="text-base font-semibold">
-                    Brand: <span>{product.brand}</span>
+                    Brand: <span>{product.productBrand}</span>
                   </p>
                   <p className="text-base font-semibold">
-                    Category: <span>{product.category}</span>
+                    Category: <span>{product.productCategory}</span>
                   </p>
                   <span className="text-xl font-bold text-primary">
-                    ${product.price.toFixed(2)}
+                    ${product.productPrice}
                   </span>
                 </div>
               </div>
 
               {/* Wishlist Button */}
-              <div className=" mt-4">
+              <div className="justify-end mt-4">
                 <Button
-                  title="Add to Wishlist"
+                  title="Add to Cart"
                   stl={
-                    "bg-primary border-primary duration-1000 hover:bg-transparent hover:border-primary hover:text-primary hover:duration-1000"
+                    "bg-primary border-primary duration-1000 hover:bg-transparent hover:border-primary hover:text-primary hover:duration-1000 w-full"
                   }
                 />
               </div>
--- src/components/Product/ProductData.jsx
+++ src/components/Product/ProductData.jsx
@@ -1,9 +1,22 @@
-import { products } from "../../data/Data";
+import { useQuery } from "@tanstack/react-query";
+
 import Button from "../Shared/Button";
+import axios from "axios";
+import Loading from "../Shared/Loading";
 
 const ProductData = () => {
 
-  const maxLength = 18;
+  // const maxLength = 18;
+  const { data: products = [], isLoading} = useQuery({
+    queryKey: "products",
+    queryFn: async () => {
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
+      return data;
+    }
+  })
+
+  // Showing Loading Screen if data not found
+  if (isLoading) return <Loading />
 
   return (
     <div >
@@ -14,42 +27,35 @@ const ProductData = () => {
             key={product.id}
             className="border border-primary p-6 rounded-lg min-h-60"
           >
-            <div className="flex flex-col justify-between gap-5 h-fit">
+            <div className="flex flex-col justify-between gap-5">
               <div>
                 <figure className="flex justify-center">
                   <img
-                    src={product.image}
-                    alt={product.name}
-                    className="w-28 h-28 rounded-md"
+                    src={product.productImage}
+                    alt={product.productName}
+                    className="w-40 h-40 rounded-md"
                   />
                 </figure>
                 <div className="flex flex-col space-y-3 mt-6">
-                  <h3 className="text-lg font-bold text-slate-700">
-                    {
-                      product.title.length > maxLength ? (
-                        product.title.slice(0, maxLength) + "..."
-                      ) : (
-                        product.title
-                      )
-                    }
-                    
+                  <h3 className="text-xl font-bold text-slate-700">
+                    {product.productName}
                   </h3>
                   <p className="text-base font-semibold">
-                    Brand: <span>{product.brand}</span>
+                    Brand: <span>{product.productBrand}</span>
                   </p>
                   <p className="text-base font-semibold">
-                    Category: <span>{product.category}</span>
+                    Category: <span>{product.productCategory}</span>
                   </p>
                   <span className="text-xl font-bold text-primary">
-                    ${product.price.toFixed(2)}
+                    ${product.productPrice}
                   </span>
                 </div>
               </div>
 
               {/* Wishlist Button */}
-              <div className="w-full mt-4">
+              <div className="justify-end mt-4">
                 <Button
-                  title="Add to Wishlist"
+                  title="Add to Cart"
                   stl={
                     "bg-primary border-primary duration-1000 hover:bg-transparent hover:border-primary hover:text-primary hover:duration-1000 w-full"
                   }
