1fbd761877e6bcb1d0098670d6578b22fdb1b843
codewithsaidul <codewithsaidul@gmail.com>
2024-11-22 09:15:46 +0600

    User Data Updet Modal & Funconality added

---
+++ src/components/Dashboard/Modal.jsx
@@ -0,0 +1,101 @@
+import axios from "axios";
+import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+
+
+const Modal = ({ user, setModalVisible, refetch }) => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const handleUpdate = async (data) => {
+        const role = data.role;
+        const status = data.status;
+        const updatedUser = {...user, role, status };
+        try {
+            setModalVisible(false);
+           const { data } = await axios.patch(`${import.meta.env.VITE_API_URL}/userUpdate/${user._id}`, updatedUser);
+
+           if (data.modifiedCount > 0) {
+            refetch();
+            Swal.fire({
+              position: "top-center",
+              icon: "success",
+              title: "Updated Successfully",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+
+            
+        } catch (error) {
+            Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: error.message,
+                showConfirmButton: false,
+                timer: 1500,
+              });
+        }
+    }
+
+  return (
+    <div className="modalStatus mt-20">
+      <div className="modal__content">
+        <button className="modalClose" onClick={() => setModalVisible(false)}>
+          &times;
+        </button>
+        <form onSubmit={handleSubmit(handleUpdate)} className="mt-5">
+          <div className="flex items-center gap-2 w-full">
+              <div className="w-full">
+                <h2 className="mb-2">Role</h2>
+                <select className="select select-bordered w-full"
+                {...register("role")}
+                >
+                  <option value={user?.role === "buyer" ? "buyer" : "seller"}>
+                    {user?.role === "buyer" ? "buyer" : "seller"}
+                  </option>
+                  <option value={user?.role === "buyer" ? "seller" : "buyer"}>
+                    {user?.role === "buyer" ? "seller" : "buyer"}
+                  </option>
+                </select>
+              </div>
+
+              <div className="w-full">
+                <h2 className="mb-2">Status</h2>
+                <select className="select select-bordered w-full" 
+                {...register("status", {required: true})}
+                >
+                  <option value={user?.role === "buyer" ? "Approved" : "Pending"}>
+                  {user?.role === "buyer" ? "Approved" : "Pending"}
+                  </option>
+                  <option value={user?.role === "seller" ? "Approved" : "Pending"}>
+                    {user?.role === "seller" ? "Approved" : "Pending"}
+                  </option>
+                </select>
+                {
+                    errors.status && (
+                    <p className="text-sm my-2 font-bold text-red-700">
+                        {"Status is required"}
+                    </p>
+                  )
+                }
+              </div>
+          </div>
+
+          <button className="py-2 px-4 bg-primary text-white rounded-lg mt-3">
+            Update Data
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  setModalVisible: PropTypes.func,
+  user: PropTypes.object,
+  refetch: PropTypes.func
+};
+
+export default Modal;
--- src/pages/Dashboard/AllUser.jsx
+++ src/pages/Dashboard/AllUser.jsx
@@ -6,13 +6,20 @@ import { MdOutlineDeleteForever } from "react-icons/md";
 import useUserData from "../../hooks/useUserData";
 // import { useState } from "react";
 import Swal from "sweetalert2";
+import Modal from "../../components/Dashboard/Modal";
+import { useState } from "react";
 
 const AllUser = () => {
   const userData = useUserData();
-
+  const [user, setUser] = useState()
+  const [isModalVisible, setModalVisible] = useState(false);
 
   // Get the all user from db
-  const { refetch, data: users = [], isLoading } = useQuery({
+  const {
+    refetch,
+    data: users = [],
+    isLoading,
+  } = useQuery({
     queryKey: "users",
     queryFn: async () => {
       const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/users`);
@@ -22,7 +29,6 @@ const AllUser = () => {
 
   //   Get The All User Length
   const userLength = users.length;
-
 
   //   Delete User From Database
   const handleDeleteUser = async (userId, role) => {
@@ -41,7 +47,6 @@ const AllUser = () => {
         `${import.meta.env.VITE_API_URL}/user/${userId}`
       );
 
-
       if (data.deletedCount > 0) {
         refetch();
         Swal.fire({
@@ -52,10 +57,7 @@ const AllUser = () => {
           timer: 1500,
         });
       }
-      
-
-      
-    } catch (error) {;
+    } catch (error) {
       Swal.fire({
         position: "top-center",
         icon: "error",
@@ -64,7 +66,27 @@ const AllUser = () => {
         timer: 1500,
       });
     }
-   
+  };
+
+  const toggleModal = (userId, role) => {
+    if (userData?.role === role) {
+      return Swal.fire({
+        position: "top-center",
+        icon: "error",
+        title: "Cann't Change your Status or Role",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+
+    if (userData?.role !== role) {
+      setModalVisible(!isModalVisible);
+
+      const userData = users.find(user => user._id === userId);
+      setUser(userData);
+    }
+
+
   };
 
 
@@ -74,7 +96,7 @@ const AllUser = () => {
   return (
     <div>
       {/* All User Container */}
-      <div>
+      <div className="relative">
         {userLength > 0 ? (
           <div>
             <h2 className="text-2xl font-bold text-slate-700 mt-6 mb-10 text-center  ">
@@ -112,7 +134,10 @@ const AllUser = () => {
                         {user.status}
                       </td>
                       <td className="flex items-center gap-4">
-                        <button className="text-blue-700 font-bold">
+                        <button
+                          onClick={() => toggleModal(user._id, user?.role)}
+                          className="text-blue-700 font-bold"
+                        >
                           <FaRegEdit size={20} />
                         </button>
                         <button
@@ -127,6 +152,12 @@ const AllUser = () => {
                 </tbody>
               </table>
             </div>
+
+            {isModalVisible && (
+              <div>
+                <Modal user={user} refetch={refetch} setModalVisible={setModalVisible} />
+              </div>
+            )}
           </div>
         ) : (
           <div className="flex justify-center items-center min-h-screen">
@@ -136,6 +167,8 @@ const AllUser = () => {
           </div>
         )}
       </div>
+
+      {/* ============= Modal for select user role & status */}
     </div>
   );
 };
